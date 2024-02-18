ac905d0022d942d467c175ef8d19f2088cff3216
yoann.pages <yoann.pages@ecole2600.com>
2024-02-18 18:39:31 +0100

    -d sans arguments - Yoann

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int my_strlen(const char *str) {
 // Cette fonction retourne bool:
 // Elle a comme paramètres : un boolean en detailflag et une chaine de caratère pour filtrer les processus par leur nom
 BOOL print_process(BOOL detailFlag, const char* filterName) {
+    printf("\n");
     // Elle créée un clicjés instantané sur les processus en cours d'execution
     // Le second argument, 0, spécifie que l'instantané concerne tous les processus du système. La fonction retourne un HANDLE vers l'instantané créé.
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -39,7 +40,7 @@ BOOL print_process(BOOL detailFlag, const char* filterName) {
     
     //cette ligne imprime l'en-tête d'une table qui sera utilisé pour afficher les informations sur chaque processus
     //Les spécificateurs de format dans printf sont utilisés pour allouer un espace fixe à chaque colonne.
-    printf("| %-32s | %6s | %4s | %4s | %4s | %12s | %12s |\n", "Process Name", "PID", "Pri", "Thd", "Hnd", "CPU Time", "Elapsed Time");
+    printf("| %-32s | %6s | %4s | %4s | %7s | %14s | %12s |\n", "Process Name", "PID", "Pri", "Thd", "Hnd", "CPU Time", "Elapsed Time");
 
     // on utilise SYSTEMTIME pour représenter une date et une heure dans un format facilement compréhensible( année, mois , jour, heures, minutes, secondes et les milisecondes)
 	SYSTEMTIME st;
@@ -87,44 +88,118 @@ do {
                     UINT eMinutes = (UINT)((processElapsedTimeMillisec / (1000 * 60)) % 60);
                     UINT eSeconds = (UINT)((processElapsedTimeMillisec / 1000) % 60);
 
-                    printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %02u:%02u:%02u.%03u | %02u:%02u:%02u |\n",
+                    printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %2c%02u:%02u:%02u.%03u | %4c%02u:%02u:%02u |\n",
                            pe32.szExeFile,
                            pe32.th32ProcessID,
                            pe32.pcPriClassBase,
                            pe32.cntThreads,
                            handleCount,
-                           hours, minutes, seconds, milliseconds,
-                           eHours, eMinutes, eSeconds);
+                           ' ',hours, minutes, seconds, milliseconds,
+                           ' ',eHours, eMinutes, eSeconds);
                 } else {
-					printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %02u:%02u:%02u.%03u | %02u:%02u:%02u |\n",
+					printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %2c%02u:%02u:%02u.%03u | %4c%02u:%02u:%02u |\n",
                            pe32.szExeFile,
                            pe32.th32ProcessID,
                            pe32.pcPriClassBase,
                            pe32.cntThreads,
                            0,
-                           0,0,0,0,
-                           0,0,0);
+                           ' ',0,0,0,0,
+                           ' ',0,0,0);
                 }
                 CloseHandle(hProcess);
             } else {
-                    printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %02u:%02u:%02u.%03u | %02u:%02u:%02u |\n",
+                    printf("| %-32s | %6lu | %4ld | %4lu | %7lu | %2c%02u:%02u:%02u.%03u | %4c%02u:%02u:%02u |\n",
                        pe32.szExeFile,
                        pe32.th32ProcessID,
                        pe32.pcPriClassBase,
                        pe32.cntThreads,
                        0,
-                       0,0,0,0,
-                       0,0,0);
+                       ' ',0,0,0,0,
+                       ' ',0,0,0);
             }
         }
     } while (Process32Next(hSnapshot, &pe32));
 
     CloseHandle(hSnapshot);
+    printf("\n");
     // printf("Snapshot of processes completed successfully.\n");
     return TRUE;
 }
 
-BOOL print_thread_details(DWORD processID) {
+// ici on print les details de tous threads si -d est utilisé sans argument
+BOOL print_all_thread_details(BOOL detailFlag, const char* filterName)
+{
+    printf("\n");
+    // Elle créée un clicjés instantané sur les processus en cours d'execution
+    // Le second argument, 0, spécifie que l'instantané concerne tous les processus du système. La fonction retourne un HANDLE vers l'instantané créé.
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE) {
+        printf("Failed to create process snapshot.\n");
+        return FALSE;
+    }
+
+    //ici PROCESSENTRY32  déclare une varibale pe32. Cette strcuture contient plusieurs champs pouvant stocker des informations sur le processus (PID, Threads etc..)
+    PROCESSENTRY32 pe32;
+    //Cette étape est obligatoire : elle servira a utiliser Process32First ou Process32Next avec pe32
+    //dwSize est déninie avec la taille de la structure de PROCESSENTRY32
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Le Process32Firsrt se remplit  par la suite
+    // Si le Process32First n'existe pas alors, il fermera le Handle et passera au prochaine processus
+    if (!Process32First(hSnapshot, &pe32)) {
+        printf("Failed to get the first process.\n");
+        CloseHandle(hSnapshot);
+        return FALSE;
+    }
+
+    //tant que des processus existent on affiche les informations detaillees sur les threads actuel
+    do {
+        HANDLE hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+        if(hThreadSnap == INVALID_HANDLE_VALUE) {
+            return FALSE;
+        }
+        THREADENTRY32 te32;
+        te32.dwSize = sizeof(THREADENTRY32);
+        if(!Thread32First(hThreadSnap, &te32)) {
+            CloseHandle(hThreadSnap);
+            return FALSE;
+        }
+        printf("Name: %s\n", pe32.szExeFile);
+        printf("| %-6s | %-3s | %-6s | %-6s | %-12s | %-12s | %-12s |\n", "Tid", "Pri", "Cswtch", "State", "User Time", "Kernel Time", "Elapsed Time");
+        do {
+            if (te32.th32OwnerProcessID == pe32.th32ProcessID) { // Check if the thread belongs to the current process
+                HANDLE hThread = OpenThread(THREAD_QUERY_INFORMATION, FALSE, te32.th32ThreadID);
+                FILETIME ftCreation, ftExit, ftKernel, ftUser;
+                SYSTEMTIME stKernel, stUser;
+                ULARGE_INTEGER uiKernel, uiUser;
+                if(hThread && GetThreadTimes(hThread, &ftCreation, &ftExit, &ftKernel, &ftUser)) {
+                    FileTimeToSystemTime(&ftKernel, &stKernel);
+                    FileTimeToSystemTime(&ftUser, &stUser);
+                    uiKernel.LowPart = ftKernel.dwLowDateTime;
+                    uiKernel.HighPart = ftKernel.dwHighDateTime;
+                    uiUser.LowPart = ftUser.dwLowDateTime;
+                    uiUser.HighPart = ftUser.dwHighDateTime;
+                    printf("| %-6lu | %-3ld | %-6s | %-6s | %02u:%02u:%02u.%03u | %02u:%02u:%02u.%03u | %-12s |\n",
+                        te32.th32ThreadID,
+                        te32.tpBasePri,
+                        "N/A",  //pas réussi à récupérer
+                        "N/A",  //pas réussi à récupéer
+                        stUser.wHour, stUser.wMinute, stUser.wSecond, stUser.wMilliseconds,
+                        stKernel.wHour, stKernel.wMinute, stKernel.wSecond, stKernel.wMilliseconds,
+                        "N/A");  //pas réussi à récupérer
+                }
+                if (hThread) {
+                    CloseHandle(hThread);
+                }
+            }
+        } while (Thread32Next(hThreadSnap, &te32));
+        printf("\n");
+        CloseHandle(hThreadSnap);
+    } while (Process32Next(hSnapshot, &pe32));
+}
+
+BOOL print_thread_details(DWORD processID) 
+{
     HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
     THREADENTRY32 te32;
     FILETIME ftCreation, ftExit, ftKernel, ftUser;
@@ -165,11 +240,10 @@ BOOL print_thread_details(DWORD processID) {
             if (hThread) {
                 CloseHandle(hThread);
             }
-
         }
-
     } while(Thread32Next(hThreadSnap, &te32));
     CloseHandle(hThreadSnap);
+    printf("\n");
     return TRUE;
 }
 
@@ -190,8 +264,13 @@ int main(int argc, char *argv[]) {
                 pid = atoi(argv[i + 1]); 
                 i++; 
             }
-        } else {
-            filterName = argv[i];
+            else if (i + 1 == argc) {
+                pid = -1;
+            }
+        }
+        else {
+            printf("\nError in arguments: %s\nDo -h or --help for more infromations\n\n",argv[i]);
+            helpFlag = TRUE;
         }
     }
 
@@ -199,28 +278,41 @@ int main(int argc, char *argv[]) {
         // Display help
         printf("Usage: %s [options] [name]\n\n", argv[0]);
         printf("Options:\n");
-        printf(" -h \t\tDisplay this help message and exit.\n");
-        printf(" -d \t\tShow thread detail.\n");
-        printf(" name \t\tShow information about processes that begin with the name specified.\n\n");
+        printf(" -h \t\t\tDisplay this help message and exit.\n");
+        printf(" -d \t\t\tShow thread detail.\n");
+        printf(" name \t\t\tShow information about processes that begin with the name specified.\n\n");
         printf("Abbreviation key:\n");
-        printf(" PID \t\tProcess Identifier\n");
-        printf(" Pri \t\tPriority\n");
-        printf(" Thd \t\tNumber of Threads\n");
-        printf(" Hnd \t\tNumber of Handles\n");
-        printf(" CPU Time \tCPU Time in seconds\n");
+        printf(" PID \t\t\tProcess Identifier\n");
+        printf(" Pri \t\t\tPriority\n");
+        printf(" Thd \t\t\tNumber of Threads\n");
+        printf(" Hnd \t\t\tNumber of Handles\n");
+        printf(" CPU Time \t\tCPU Time in seconds\n");
         printf(" Elapsed Time \t\tElapsed Time in seconds\n");
+        printf("\n");
         return 0;
     }
 
-    if (detailFlag && pid != 0) {
-        if (!print_thread_details(pid)) { // Correction ici
-            printf("An error occurred while printing the thread list.\n");
+    if(detailFlag) {
+        if (pid == -1)
+        {
+            if (!print_all_thread_details(detailFlag, filterName)) {
+                printf("An error occurred while printing the thread list.\n");
+                printf("\n");
+            }
+            return 0;
         }
-        return 0; 
-    }   
+        if (pid != 0) {
+            if (!print_thread_details(pid)) { // Correction ici
+                printf("An error occurred while printing the thread list.\n");
+                printf("\n");
+            }
+            return 0; 
+        }   
+    }
 
     if (!print_process(detailFlag, filterName)) {
         printf("An error occurred while printing the process list.\n");
+        printf("\n");
     }
     return 0;
 }
